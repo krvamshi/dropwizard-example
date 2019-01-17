5e0adb4b62b840627a0b4e2ef210ae8cf1d53dd9
Vamsi <vamsikrishna.krishnamraju@gmail.com>
2019-01-17 14:36:38 +0000

    Account service created

--- src/main/java/DropwizardExampleApplication.java
+++ src/main/java/DropwizardExampleApplication.java
@@ -1,3 +1,4 @@
+import com.training.service.AccountService;
 import io.dropwizard.Application;
 import io.dropwizard.Configuration;
 import io.dropwizard.setup.Environment;
@@ -10,5 +11,6 @@ public class DropwizardExampleApplication extends Application<Configuration> {
   @Override
   public void run(Configuration configuration, Environment environment) {
     environment.jersey().register(new Resource());
+    environment.jersey().register(new AccountService());
   }
 }
